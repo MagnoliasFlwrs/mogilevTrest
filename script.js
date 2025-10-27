5399bf8644b32233ad1bd54feee6a05a5696ae12
savenok <magnolia.sav@gmail.com>
2025-10-27 23:33:59 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+const partnersSwiper = new Swiper('.partners-swiper', {
+    loop: true,
+    centeredSlides: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+        450: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        },
+        1200: {
+            slidesPerView: 5
+        }
+    }
+});
+
+const gallerySwiper = new Swiper('.gallery-swiper', {
+    slidesPerView: 1,
+    navigation: {
+        nextEl: '.gallery-swiper-container .custom-swiper-button-next',
+        prevEl: '.gallery-swiper-container .custom-swiper-button-prev',
+    },
+});
+
+// custom select
+
+const servicesSelect =  document.querySelector('.services');
+const servicesOptions = document.querySelectorAll('.services-list li');
+const servicesSelectBody = document.querySelector('.services-list');
+
+if (servicesSelect) {
+    servicesSelect.addEventListener('click' , () => {
+        servicesSelectBody.classList.add('active');
+    })
+    servicesSelect.querySelector('p').addEventListener('click' , () => {
+        servicesSelectBody.classList.add('active');
+    })
+    servicesOptions.forEach(el => {
+        el.addEventListener('click' , ()=> {
+            servicesSelect.querySelector('p').innerHTML = el.innerHTML;
+            servicesSelect.dataset.current = el.dataset.value;
+            servicesSelectBody.classList.remove('active')
+        })
+    })
+    document.addEventListener('click' , (e)=> {
+        if ((!e.target.closest('.services-wrap')) && servicesSelectBody.classList.contains('active')) {
+            servicesSelectBody.classList.remove('active');
+        }
+    })
+}
+
