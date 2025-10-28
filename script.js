83b0f97b7b5e6ea175fde3266c46464344b56e31
savenok <magnolia.sav@gmail.com>
2025-10-28 23:21:43 +0300

    upd: header, contacts

--- script.js
+++ script.js
@@ -51,3 +51,26 @@ if (servicesSelect) {
     })
 }
 
+
+// burger
+
+const burgerBtn = document.querySelector('.burger-btn');
+const burgerMenu = document.querySelector('.burger-menu');
+const specialBurgerBtn = document.querySelector('.special-burger')
+burgerBtn?.addEventListener('click' , ()=> {
+    burgerBtn.classList.toggle('active');
+    burgerMenu.classList.toggle('open');
+    if (burgerMenu.classList.contains('open')) {
+        document.querySelector('body').style.overflow = 'hidden';
+    } else {
+        document.querySelector('body').style.overflow = 'auto';
+    }
+})
+specialBurgerBtn?.addEventListener('click' , ()=> {
+    burgerMenu.classList.toggle('open');
+    if (burgerMenu.classList.contains('open')) {
+        document.querySelector('body').style.overflow = 'hidden';
+    } else {
+        document.querySelector('body').style.overflow = 'auto';
+    }
+})
